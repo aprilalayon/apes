85487c45fd7a37d085bd9bbc4a55efc82c0279e3
April Alayon <eatingaprilapple@gmail.com>
2022-10-21 15:27:29 -0700

    Add vue router and create navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
  <script setup>
-import Header from './components/Header.vue'
+import Navigation from '@/components/Navigation.vue'
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <Header title="Pilot site" />
-      <nav id="nav">
-          <router-link to="/">Home</router-link> |
-          <router-link to="/about">About</router-link> |
-          <router-link to="/projects">Projects</router-link>
-      </nav>
+      <Navigation />
       <router-view />
     </div>
   </header>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/projects">Projects</router-link>
+    </nav>
+
+</template>
+
+<script setup>
+    defineProps({
+        title: {
+        type: String,
+        required: true
+        }
+    })
+</script>
+
+<style scoped>
+    h1 {
+        font-family: "Domine-Bold", Helvetica, Arial;
+        font-size: 2.6rem;
+    }
+
+</style>
+  
