a2cdc7dca23aa8f23d250fda73c80eac0048a2be
April Alayon <eatingaprilapple@gmail.com>
2022-10-25 13:23:35 -0700

    Style intro page and add typography

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,30 +1,28 @@
 <template>
-    <div class="header">
-      <p>Hi, my name is</p>
-      <h1>{{ name }}</h1>
-      <p>
-        I'm a front end engineer dedicated to designing and building accessible and functional digital experiences.
-      </p>
-    </div>
+  <div class="intro">
+    <p>Hi, my name is</p>
+    <h1>{{ name }}</h1>
+    <p>
+      I'm a front end engineer dedicated to designing and building accessible and functional digital experiences.
+    </p>
+  </div>
+</template>
   
-  </template>
-  
-  <script setup>
-    defineProps({
-      name: {
-        type: String,
-        required: true
-      }
-    })
-  </script>
-  
-  <style scoped>
-  h1 {
-    font-family: "Domine-Bold", Helvetica, Arial;
-    font-weight: 500;
-    font-size: 2.6rem;
-    top: -10px;
-  }
+<script setup>
+  defineProps({
+    name: {
+      type: String,
+      required: true
+    }
+  })
+</script>
 
-  </style>
-  
+<style scoped>
+h1 {
+  font-family: "Domine-Bold", Helvetica, Arial;
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+</style>
