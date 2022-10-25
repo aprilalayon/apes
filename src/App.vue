bdb1c6400ab2f1d7179e153f4a9824f2dd279115
April Alayon <eatingaprilapple@gmail.com>
2022-10-25 13:22:13 -0700

    Add router link for the logo

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
  <script setup>
 import Navigation from '@/components/Navigation.vue'
+import logo from '@/assets/images/logo.svg'
 </script>
 
 <template>
   <header>
-    <div class="wrapper">
-      <Navigation />
-      <router-view />
-    </div>
+    <router-link to="/">
+      <img 
+        class="logo"
+        alt="logo"
+        :src="logo"
+      />
+    </router-link>
+    <Navigation />
   </header>
+  <div class="wrapper">
+    <router-view />
+  </div>
 </template>
 
-<style lang="scss" scoped>
+<style scoped>
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 
+.logo {
+  width: 80px;
+  height: 80px;
+}
 </style>
