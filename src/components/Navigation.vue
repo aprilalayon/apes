7a8eeb6ecbea3bdee7df712d1c53099b21f49a0a
April Alayon <eatingaprilapple@gmail.com>
2022-10-27 13:21:19 -0700

    Update navigation styles

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,21 +1,33 @@
 <template>
-    <nav id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link> |
+    <nav id="nav" class="nav">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
         <router-link to="/projects">Projects</router-link>
     </nav>
 
 </template>
 
-<script setup>
-
-</script>
-
 <style scoped>
-    h1 {
-        font-family: "Domine-Bold", Helvetica, Arial;
-        font-size: 2.6rem;
-    }
+.nav {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    max-width: 300px;
+}
+
+nav a {
+    font-family: "Poppins-Bold", Helvetica, Arial;
+    text-decoration: none;
+    color: #568a93;
+}
+
+nav a:hover {
+    color: #c76f47;
+    text-decoration: underline;
+    text-decoration-color: #c76f47;
+    text-decoration-thickness: 6px;
+    text-underline-offset: 10px;
+}
 
 </style>
   
