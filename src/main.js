231ffc9913cb294ac3d3000bdcb9bc7aec7679c3
April Alayon <eatingaprilapple@gmail.com>
2022-10-28 00:11:31 -0700

    Add font awesome library for icons

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faToolbox, faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+import "@/assets/styles/main.scss"
 
-//import your scss here
-import "@/assets/styles/main.scss";
+library.add(
+    faUpRightFromSquare,
+    faToolbox
+)
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+.use(router)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
